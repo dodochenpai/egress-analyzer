22a7fae0ecfd67bd5ae4d7bcf19cbd6836b33c2d
dodochenpai <72485122+dodochenpai@users.noreply.github.com>
2022-11-04 09:46:49 -0700

    Corrected open port count issued caused by multithreading

--- egress.cpp
+++ egress.cpp
@@ -9,6 +9,7 @@
 #include <string>
 #include <thread>
 #include <vector>
+#include <future>
 
 WSADATA wsaData;
 SOCKET wSock;
@@ -20,15 +21,12 @@ void print_usage();
 
 using namespace std;
 
-// Global Statistics
-int globalOpenCount = 0;
-int globalClosedCount = 0;
-
 int main(int argc, char *argv[]){
     // Create default variable values
     vector<thread> threads;
+    vector<future<int>> futures;
     int numThreads = 10;
-    int portStart = 0;
+    int portStart = 1;
     int portEnd = 1024;    
 
     // Convert command line arguments to strings
@@ -66,18 +64,22 @@ int main(int argc, char *argv[]){
     // Create threads
     cout << "Starting " << numThreads << " thread(s)\n";
     for(int i=0;i<numThreads;i++){
-        threads.push_back(thread(thread_handler, i+portStart, numThreads, portEnd));
+        // threads.push_back(thread(thread_handler, i+portStart, numThreads, portEnd));
+        futures.push_back(async(thread_handler, i+portStart, numThreads, portEnd));
     }
 
     // Wait for threads to finish before exiting
     for (int i=0;i<numThreads;i++){
-        threads[i].join();
+        //threads[i].join();
+        futures[i].wait();
     }
-
+    int openCount = 0;
+    for (int i=0; i<futures.size();i++){
+        openCount += futures[i].get();
+    }
     // Print out some statistics
-    cout << "Open ports: " << globalOpenCount << endl;
-    cout << "Closed ports: " << globalClosedCount << endl;
-    //cout << "See outfile for more output" << endl;
+    cout << "Open ports: " << openCount << endl;
+    // cout << "See outfile for more output" << endl;
 
     WSACleanup();
     return 0;
@@ -85,16 +87,32 @@ int main(int argc, char *argv[]){
 }
 
 // Calculates the ports each thread will scan
+// Returns total number of open ports
 int thread_handler(int initial, int numThreads, int max){
     // Set variable to initial value
+    int openCount = 0;
+    int closedCount = 0;
     int i = initial;
+    int retvalue;
     while (i<=max){
         // Scan the port
-        test_port(i);
+        retvalue = test_port(i);
+        // Retry if port is closed
+        
+        for (int j=0;j<3;j++){
+            if (retvalue == 0){
+                openCount++;
+                break;
+            } else {
+                cout << "Connection failed on port " << i << endl;
+                retvalue = test_port(i);
+            }
+            closedCount++;
+        }
         // Set next port to scan
         i += numThreads;
     }
-    return 0;
+    return openCount;
 }
 
 int test_port(int port){
@@ -103,6 +121,7 @@ int test_port(int port){
     struct sockaddr_in sockaddr;
     struct timeval tv;
     int result;
+    int retvalue = 1;
 
     // Set Options
     // Google Test
@@ -121,6 +140,7 @@ int test_port(int port){
     wSock = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL, (unsigned int)NULL, (unsigned int)NULL);
     if (wSock == INVALID_SOCKET) {
         cout << "Socket Error" << endl;
+        closesocket(wSock);
         return 1;
     }
     // Set socket to non-blocking
@@ -128,13 +148,15 @@ int test_port(int port){
     result = ioctlsocket(wSock, FIONBIO, &mode);
     if (result != 0){
         cout << "I/O Mode Error" << endl;
+        closesocket(wSock);
         return 1;
     }
     // Connect to address
     result = WSAConnect(wSock, (SOCKADDR*)&sockaddr, sizeof(sockaddr), NULL, NULL, NULL, NULL);
     if (result == SOCKET_ERROR){
         if (WSAGetLastError() != WSAEWOULDBLOCK){
-            cout << "Connect Error" << endl;
+            cout << "Connect Error" << ' ' << WSAGetLastError() << endl;
+            closesocket(wSock);
             return 1;
         }
 
@@ -145,20 +167,22 @@ int test_port(int port){
         FD_SET(wSock, &fds);
         result = select(0, NULL, &fds, NULL, &tv);
         if (result <= 0){
-            //cout << "Port " << port << " is not open" << endl;
-            globalClosedCount++;
+            // cout << "Port " << port << " is not open" << endl;
+            retvalue = 1;
         } else {
             //cout << "Port " << port << " is open" << endl;
-            globalOpenCount++;
+            retvalue = 0;
+            // Disconnect socket
+            result = WSASendDisconnect(wSock, NULL);
+            if (result != 0){
+                cout << "Disconnect Error" << endl;
+                closesocket(wSock);
+                return 1;
+            }
         }
     }
-    // Disconnect socket
-    result = WSASendDisconnect(wSock, NULL);
-    if (result != 0){
-        cout << "Disconnect Error" << endl;
-        return 1;
-    }
-    return 0;
+    closesocket(wSock);
+    return retvalue;
 }
 
 void print_usage(){
